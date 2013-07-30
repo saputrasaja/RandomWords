8a0b68cdd98acaed2647a8cc7bf35436a321df7e
unknown <wiwit@.(none)>
2013-07-30 05:24:34 -0800

    create layout new word

--- gen/com/wiwit/all/R.java
+++ gen/com/wiwit/all/R.java
@@ -24,35 +24,43 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout01=0x7f08000f;
-        public static final int action_settings=0x7f080013;
+        public static final int LinearLayout01=0x7f080016;
+        public static final int action_settings=0x7f08001a;
         public static final int delete_word=0x7f08000b;
-        public static final int editText1=0x7f08000e;
+        public static final int done_new=0x7f080013;
+        public static final int editText1=0x7f080015;
+        public static final int edit_new=0x7f080014;
+        public static final int english_new=0x7f08000f;
+        public static final int indonesian_new=0x7f080011;
         public static final int info_btn_refresh=0x7f08000d;
         public static final int info_delete_word=0x7f08000a;
         public static final int info_new_word=0x7f080004;
         public static final int info_old_word=0x7f080007;
         public static final int info_total_word=0x7f080001;
         public static final int new_word=0x7f080005;
+        public static final int next_new=0x7f080012;
         public static final int old_word=0x7f080008;
-        public static final int progressBar1=0x7f080012;
-        public static final int spinner1=0x7f080011;
-        public static final int subTab1=0x7f080010;
+        public static final int pass_new=0x7f080010;
+        public static final int progressBar1=0x7f080019;
+        public static final int spinner1=0x7f080018;
+        public static final int subTab1=0x7f080017;
         public static final int tableRow1=0x7f080000;
         public static final int tableRow2=0x7f080003;
         public static final int tableRow3=0x7f080006;
         public static final int tableRow4=0x7f080009;
         public static final int tableRow5=0x7f08000c;
+        public static final int toggle_new=0x7f08000e;
         public static final int total_word=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int info_layout=0x7f030001;
-        public static final int new_words_activity=0x7f030002;
-        public static final int old_words_activity=0x7f030003;
-        public static final int tab=0x7f030004;
-        public static final int tab_host_activity=0x7f030005;
-        public static final int tabs1=0x7f030006;
+        public static final int new_word_layout=0x7f030002;
+        public static final int new_words_activity=0x7f030003;
+        public static final int old_words_activity=0x7f030004;
+        public static final int tab=0x7f030005;
+        public static final int tab_host_activity=0x7f030006;
+        public static final int tabs1=0x7f030007;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/wiwit/acitivity/tab/NewWordTab.java
+++ src/com/wiwit/acitivity/tab/NewWordTab.java
@@ -1,15 +1,23 @@
 package com.wiwit.acitivity.tab;
 
 import com.wiwit.all.R;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class NewWordTab extends Activity {
-	/** Called when the activity is first created. */
+	TextView englishWord;
+	TextView indonesianWord;
+	Button pass;
+	Button next;
+	Button edit;
+	Button done;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.tabs1);
+		setContentView(R.layout.new_word_layout);
 	}
 }
